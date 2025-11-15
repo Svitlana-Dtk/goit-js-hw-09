368df3d9e950022d8cd7374fe80128da3cdf48be
Svitlana <svetlyashka2014@gmail.com>
2025-11-16 00:30:14 +0200

    HW9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,40 @@
-console.log("form");
+let formData = {
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+
+const savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+
+  form.elements.email.value = formData.email || "";
+  form.elements.message.value = formData.message || "";
+}
+
+form.addEventListener("input", event => {
+  const fieldName = event.target.name;
+
+    formData[fieldName] = event.target.value.trim();
+  
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    return alert("Fill please all fields");
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+
+  formData = { email: "", message: "" };
+});
